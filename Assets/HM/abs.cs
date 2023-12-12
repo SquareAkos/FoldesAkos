d83405806201fd4c91e376d357814c6ab8e8e2f0
SquareAkos <akos.foldes2001@gmail.com>
2023-12-12 19:26:08 +0100

    floor+ceil

--- Assets/HM/abs.cs
+++ Assets/HM/abs.cs
@@ -5,11 +5,24 @@ public class abs : MonoBehaviour
 
     void Start()
     {
-        float test = Floorown(-13.9f);
-        Debug.Log(test);
+        float testfloor = Floorown(-13.9f);
+        float t2floor = Mathf.Floor(-13.9f);
+        float testCeil = Ceiling(12.7f);
+        float originceil = Mathf.Ceil(12.7f);
+        Debug.Log(testfloor);
+        Debug.Log(t2floor);
     }
     float Floorown(float floornumber)
     {
-        int helpvar = (int)floornumber; return helpvar;
+        int helpvar = (int)floornumber;
+        if (helpvar < 0) helpvar--;
+        return helpvar;
+    }
+
+    float Ceiling(float ceilingnumber)
+    {
+        int helpvar = (int)ceilingnumber;
+        if (helpvar > 0) helpvar++;
+        return helpvar;    
     }
 }
